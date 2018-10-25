b9521aeec5fafe94a8a44b692123cb061d1947df
Sancho Day <sanchoday@gmail.com>
2018-10-25 12:19:33 -0400

    connected servers

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,7 +1,27 @@
 <template>
-    <div class="alert alert-success">
-        Yay! We have a game!
+<div class="alert alert-success">
+    Yay! We have a game!
+
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..." alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+            </div>
+        </div>
+        <div class="col-md-4"></div>
+        <div class="col-md-4"></div>
     </div>
+</div>
 </template>
 
 <style lang="scss">
@@ -9,7 +29,25 @@
 </style>
 
 <script>
+import {GetState, FlipPicture} from '@/services/api_access'
 export default {
-    
+    data: function(){
+        return {
+            state: {
+                picture: ""
+            }
+        }
+    },
+    created: function(){
+        GetState()
+            .then(x => this.state = x);
+    },
+    methods: {
+        flipPicture: function(){
+            FlipPicture()
+                .then(x => GetState())
+                .then(x => this.state = x)
+        }
+    }
 }
 </script>
